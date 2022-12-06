ffa9e63e0412b02735d6343fe00b0dc846052f82
Rember  the great <rembermaguinsay@gmail.com>
2022-12-06 20:20:06 +0800

    some changes

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,12 +2,12 @@
 <div>
   <template>
       <page-title-container
-      page_desc="page_desc about"
+      page_desc="This is our collection of templates and designs for our projects"
     />
   </template>  
 
-
-
+<h4>Tech that was used. amin nga libraries ma inayon ditoy ket ma ikarga ditoy about</h4>
+vuetify v2.x.x, php, laravel 8, 
 </div>
 </template>
 
